b74e00fbb57bb5e33d791dfcc756e57bfccbcf75
simonpltzw <simon.35@gmx.de>
2024-03-31 22:42:51 +0200

    feat(ms-login): Deleted webapi state and re-generated with ms login

---
+++ webapi/FFischbach.Events.API/Program.cs
@@ -0,0 +1,79 @@
+using FFischbach.Events.API.Services;
+using FFischbach.Events.API.Services.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+
+namespace FFischbach.Events.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("msid", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.OAuth2,
+                    Flows = new Microsoft.OpenApi.Models.OpenApiOAuthFlows
+                    {
+                        Implicit = new Microsoft.OpenApi.Models.OpenApiOAuthFlow
+                        {
+                            AuthorizationUrl = new Uri("https://login.microsoftonline.com/a21b658e-30c5-4bc5-8409-1729b686c215/oauth2/v2.0/authorize"),
+                            Scopes = new Dictionary<string, string>
+                            {
+                                { "api://ee995dcc-a9ec-4203-93ea-81b5f8621033/access_as_user", "access_as_user" }
+                            }
+                        }
+                    }
+                });
+
+                c.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference { Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme, Id = "msid" }
+                        },
+                        new [] { "api://ee995dcc-a9ec-4203-93ea-81b5f8621033/access_as_user" }
+                    }
+                });
+            });
+
+            builder.Services.AddSingleton<IEncryptionService, EncryptionService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "FFischbach.Events.API");
+                    c.OAuthClientId("979c1c0e-193c-4bb7-8024-c24c493b2e41");
+                });
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
