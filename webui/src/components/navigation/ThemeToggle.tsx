13d7c868ab564274e4ae6fe634f8a1ba20458a0f
Luka Hefter <luka.hefter@online.de>
2024-07-28 01:47:23 +0200

    feat: save theme

--- webui/src/components/navigation/ThemeToggle.tsx
+++ webui/src/components/navigation/ThemeToggle.tsx
@@ -1,26 +1,26 @@
 import { useAppSettings } from "@/context/appSettings";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-import { FC, useEffect, useState } from "react";
+import { FC } from "react";
 
 export const ThemeToggle: FC<any> = () => {
-  const [isDarkMode, setDarkMode] = useState<boolean>(false);
-  const [, setAppSettings] = useAppSettings();
+  const [appSettings, setAppSettings] = useAppSettings();
 
-  useEffect(() => {
-    setAppSettings((settings) => {
+  const toggle = (value: boolean) => {
+    localStorage.setItem("theme", value ? "dark" : "light");
+    setAppSettings((state) => {
       return {
-        ...settings,
-        isDarkMode,
+        ...state,
+        isDarkMode: value,
       };
     });
-  }, [isDarkMode]);
+  };
 
   return (
     <div>
-      {isDarkMode ? (
-        <SunIcon height={20} onClick={() => setDarkMode(false)} />
+      {appSettings.isDarkMode ? (
+        <SunIcon height={20} onClick={() => toggle(false)} />
       ) : (
-        <MoonIcon height={20} onClick={() => setDarkMode(true)} />
+        <MoonIcon height={20} onClick={() => toggle(true)} />
       )}
     </div>
   );
--- webui/src/context/appSettings.tsx
+++ webui/src/context/appSettings.tsx
@@ -1,18 +1,32 @@
 import { AppSettings } from "@/models/appSettings";
-import { createContext, Dispatch, SetStateAction, useContext, useState } from "react";
+import { createContext, Dispatch, SetStateAction, useContext, useEffect, useState } from "react";
 
-const Context = createContext<[AppSettings ,Dispatch<SetStateAction<AppSettings>>]>([{isDarkMode: false}, () => {}]);
+const Context = createContext<[AppSettings, Dispatch<SetStateAction<AppSettings>>]>([
+  { isDarkMode: false },
+  () => {},
+]);
 
-export const AppSettingsProvider = ({ children }: any) => { 
-    const [appSettings, setAppSettings] = useState<AppSettings>({isDarkMode: false})
-  
-    return <Context.Provider value={[appSettings, setAppSettings]}>{children}</Context.Provider>;
-  };
+export const AppSettingsProvider = ({ children }: any) => {
+  const [appSettings, setAppSettings] = useState<AppSettings>({ isDarkMode: false });
+
+  useEffect(() => {
+    setAppSettings((state: AppSettings) => {
+      const isDarkMode = localStorage.getItem("theme") == "dark" ? true : false;
+      console.log(isDarkMode);
+      return {
+        ...state,
+        isDarkMode,
+      };
+    });
+  }, []);
+
+  return <Context.Provider value={[appSettings, setAppSettings]}>{children}</Context.Provider>;
+};
 
 export const useAppSettings = (): [AppSettings, Dispatch<SetStateAction<AppSettings>>] => {
-    const ctx = useContext(Context)
-    if(!ctx) {
-        throw new Error('useAppSettings must be used within a AppSettingsProvider')
-    }
+  const ctx = useContext(Context);
+  if (!ctx) {
+    throw new Error("useAppSettings must be used within a AppSettingsProvider");
+  }
   return useContext(Context);
 };
