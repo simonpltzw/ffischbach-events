71993cb4e70e1fba45fe9dc9fee0219851349a43
Luka Hefter <luka.hefter@online.de>
2024-10-06 17:15:02 +0200

    fix: filter approved on csv

--- webui/src/services/dataPreparationService.tsx
+++ webui/src/services/dataPreparationService.tsx
@@ -8,11 +8,9 @@ import { getGroup } from "./groupsService";
 import useToken from "./tokenService";
 import { Participant } from "@/models/in/Participant";
 
-
 export const useJsonToCsv = () => {
   const [eventSettings] = useEventSettings();
   const { getToken } = useToken();
-
 
   //https://www.geeksforgeeks.org/how-to-convert-json-object-to-csv-in-javascript/
   const jsonToCsv = (jsonData: any) => {
@@ -36,31 +34,31 @@ export const useJsonToCsv = () => {
 
     const token = await getToken();
     const result = await Promise.all(
-      event.groups!.map(async (g: Group) => {
-        const group = await getGroup(token, g.id);
+      event
+        .groups!.filter((g: Group) => g.approved)
+        .map(async (g: Group) => {
+          const group = await getGroup(token, g.id);
 
-        const decGroup: Group = await decryptGroup(group, { privateKey });
+          const decGroup: Group = await decryptGroup(group, { privateKey });
 
-        delete decGroup.contact.encryptedData;
-        decGroup.participants.forEach((p: Participant) => {
-          delete p.encryptedData;
-        });
+          delete decGroup.contact.encryptedData;
+          decGroup.participants.forEach((p: Participant) => {
+            delete p.encryptedData;
+          });
 
-        const exportData: ExportEntry = {
-          Anzahl: decGroup.participants.length,
-          Kategorie: decGroup.category,
-          Ansprechpartner: `${decGroup.contact.FirstName} ${decGroup.contact.LastName}`,
-          groupId: decGroup.id,
-          Gruppe: decGroup.name,
-          internerKontakt: "",
-        };
+          const exportData: ExportEntry = {
+            Anzahl: decGroup.participants.length,
+            Kategorie: decGroup.category,
+            Ansprechpartner: `${decGroup.contact.FirstName} ${decGroup.contact.LastName}`,
+            groupId: decGroup.id,
+            Gruppe: decGroup.name,
+            internerKontakt: "",
+          };
 
-        return exportData;
-      })
+          return exportData;
+        })
     );
-  
-    console.log(jsonToCsv(result))
-    return jsonToCsv(result)
+    return jsonToCsv(result);
   };
 
   return {
