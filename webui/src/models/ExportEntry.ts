51ca819b790d5e824d25edf315e2014c678dde26
Luka Hefter <luka.hefter@online.de>
2024-11-11 18:17:02 +0100

    fix: changed export appearance

--- webui/src/models/ExportEntry.ts
+++ webui/src/models/ExportEntry.ts
@@ -1,14 +1,11 @@
 //participant + group info
 
 export interface ExportEntry {
-    GruppeID: number;
+    Gr: number;
+    Nr: number;
     Vorname: string;
     Nachname: string;
-    Geburtsdatum: string
     Kategorie: string;
-    KategorieID: string;
+    KategorieNr: string;
     Gruppe: string;
-    Anzahl: number;
-    Ansprechpartner: string;
-    internerKontakt: string;
 }
